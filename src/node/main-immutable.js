f59dd5857c2285a01fb42fa319e4ea28aa7cec8b
Santeri Hiltunen <accounts.github@me.hilzu.moe>
2017-11-28 23:09:56 +0200

    More modern style JS

--- src/node/main-immutable.js
+++ src/node/main-immutable.js
@@ -1,24 +1,19 @@
-"use strict";
+"use strict"
 
-const Immutable = require('immutable')
+const Immutable = require("immutable")
 
 const windowSize = 200000
 const msgCount = 1000000
 const msgSize = 1024
 
-function createMessage (n) {
-  return Buffer.alloc(msgSize, n % 256)
-}
+const createMessage = n => Buffer.alloc(msgSize, n % 256)
 
-function pushMessage (map, id) {
+const pushMessage = (map, id) => {
   const lowId = id - windowSize
   const inserted = map.set(id, createMessage(id))
-  return lowId >= 0
-    ? inserted.delete(lowId)
-    : inserted
+  return lowId >= 0 ? inserted.delete(lowId) : inserted
 }
 
 const map = new Immutable.Map()
 
-new Immutable.Range(0, msgCount).reduce(
-  (map, i) => pushMessage(map, i), map)
+new Immutable.Range(0, msgCount).reduce((map, i) => pushMessage(map, i), map)
--- src/node/main.js
+++ src/node/main.js
@@ -1,14 +1,12 @@
-"use strict";
+"use strict"
 
 const windowSize = 200000
 const msgCount = 1000000
 const msgSize = 1024
 
-function createMessage (n) {
-  return Buffer.alloc(msgSize, n % 256)
-}
+const createMessage = n => Buffer.alloc(msgSize, n % 256)
 
-function pushMessage (map, id) {
+const pushMessage = (map, id) => {
   const lowId = id - windowSize
   map.set(id, createMessage(id))
   if (lowId >= 0) {

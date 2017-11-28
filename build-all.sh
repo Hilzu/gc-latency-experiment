0c84935e93388b8555a145c7c00f6e706b4f189a
Santeri Hiltunen <accounts.github@me.hilzu.moe>
2017-11-28 22:16:42 +0200

    Output versions before building

--- build-all.sh
+++ build-all.sh
@@ -1,18 +1,24 @@
 #!/usr/bin/env bash
 
-set -e
+set -euo pipefail
 
 cd src/java/
+javac -version
 javac Main.java
 
 cd ../scala
+scalac -version
 scalac Main.scala
 
 cd ../node
-npm install
+echo -n "Node "
+node --version
+yarn install -s
 
 cd ../haskell
+stack ghc -- --version
 stack ghc -- -O2 -optc-O3 Main.hs
 
 cd ../swift
+swiftc --version
 swiftc -O Main.swift
